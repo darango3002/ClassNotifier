312daa9037ae389eddc4edd238ae53296bc95d9b
darango3002 <donaldarango@ufl.edu>
2023-11-01 13:23:42 -0400

    set up basic functionality

--- classFinder.py
+++ classFinder.py
@@ -1,64 +1,45 @@
+import os
+import time
 import requests
-import smtplib
-import sys
-
-CARRIERS = {
-    "att": "@mms.att.net",
-    "tmobile": "@tmomail.net",
-    "verizon": "@vtext.com",
-    "sprint": "@messaging.sprintpcs.com"
-}
+from twilio.rest import Client
 
 url = 'https://one.ufl.edu/apix/soc/schedule/?category=RES&term=2241&course-code=cap4053'
 
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
 
-EMAIL = 'arangodonny@gmail.com'
-PASSWORD = 'phvg ftiq svrg amrg' 
-
-
-response = requests.get(url).json()
-waitlist = response[0]['COURSES'][0]['sections'][0]['waitList']
-print(waitlist)
+def send_message(classCode):
+    message = client.messages \
+    .create(
+        body=f"{classCode} HAS A WAITLIST SPOT AVAILABLE!",
+        from_='+18557860335',
+        to='+17274656012'
+    )
 
 def getWaitList():
     response = requests.get(url).json()
     waitList = response[0]['COURSES'][0]['sections'][0]['waitList']
-    print(waitlist)
-    return waitList
+    classCode = response[0]['COURSES'][0]['code']
+    return classCode, waitList
 
-def notifyWaitList(waitList, phone_number, carrier, message):
-    if(waitlist['isEligible'] != 'N'):
-       send_message(phone_number, carrier, message)
+def notifyWaitList():
+    classCode, waitList = getWaitList()
 
-def send_message(phone_number, carrier, message):
-    recipient = phone_number + CARRIERS[carrier]
-    auth = (EMAIL, PASSWORD)
- 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(auth[0], auth[1])
-    email = f"Subject:WaitList Open\nTo:{recipient}\n{message}"
-    server.sendmail(auth[0], recipient, email)
+    if(waitList['isEligible'] != 'N'):
+       send_message(classCode)
+       exit()
+    else:
+        print('waitList Full')
 
-def send_email(recipient, message):
-    auth = (EMAIL, PASSWORD)
- 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(auth[0], auth[1])
-    email = f"Subject:WaitList Open\nTo:{recipient}\n{message}"
-    server.sendmail(auth[0], recipient, email)
- 
- 
+def main():
+    while True:
+        notifyWaitList()
+        time.sleep(60)
+
 if __name__ == "__main__":
-    # if len(sys.argv) < 4:
-    #     print(f"Usage: python3 {sys.argv[0]} <PHONE_NUMBER> <CARRIER> <MESSAGE>")
-    #     sys.exit(0)
- 
-    #phone_number = sys.argv[1]
-    #carrier = sys.argv[2]
-    recipient = sys.argv[1]
-    message = sys.argv[2]
- 
-    #send_message(phone_number, carrier, message)
-    send_email(recipient, message)
+    main()
+
+
+
+    
